6276adfbe81c26ec784d7c34e63671e59dbe6f21
Ziyad Parekh <ziyad@1stdibs.com>
2015-05-15 15:44:37 -0400

    Refactor of smelly code

--- handlers/rooms.go
+++ handlers/rooms.go
@@ -54,50 +54,18 @@ func PostRoomCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateRoomMembers(w http.ResponseWriter, r *http.Request) {
-	// This is all copy pasted and should be shared but i am lazy as fuck
-	w.Header().Set("Content-Type", "application/json")
-
-	if !isUserLoggedIn(w, r) {
-		notAllowed := errors.New("You need to be logged in to perform this action")
-		libhttp.HandleErrorJson(w, notAllowed)
-		return
-	}
-
-	rid, err := getIdFromPath(w, r)
-	if err != nil {
-		libhttp.HandleErrorJson(w, err)
-		return
-	}
-
-	currentUser := getCurrentUser(w, r)
 	db := context.Get(r, "db").(*mgo.Session)
-
 	r_db := dal.NewRoom(db)
-	room, err := r_db.GetRoomById(rid)
-	if err != nil {
-		libhttp.HandleErrorJson(w, err)
-		return
-	}
 
-	if !isUserInRoom(currentUser.Username, room.Members) {
-		notMember := errors.New("You need to be a member of the group to make updates")
-		libhttp.HandleErrorJson(w, notMember)
-		return
-	}
-
-	rs := dal.RoomModel{}
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&rs); err != nil {
-		libhttp.HandleErrorJson(w, err)
-		return
-	}
+	rs := &dal.RoomModel{}
+	rid, rs, err := genericValidation(w, r)
 
 	if db_err := r_db.UpdateRoomMembersById(rid, rs.Members); db_err != nil {
 		libhttp.HandleErrorJson(w, db_err)
 		return
 	}
 
-	room, err = r_db.GetRoomById(rid)
+	room, err := r_db.GetRoomById(rid)
 	if err != nil {
 		libhttp.HandleErrorJson(w, err)
 		return
@@ -109,49 +77,18 @@ func UpdateRoomMembers(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateRoomName(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	if !isUserLoggedIn(w, r) {
-		notAllowed := errors.New("You need to be logged in to perform this action")
-		libhttp.HandleErrorJson(w, notAllowed)
-		return
-	}
-
-	rid, err := getIdFromPath(w, r)
-	if err != nil {
-		libhttp.HandleErrorJson(w, err)
-		return
-	}
-
-	currentUser := getCurrentUser(w, r)
 	db := context.Get(r, "db").(*mgo.Session)
-
 	r_db := dal.NewRoom(db)
-	room, err := r_db.GetRoomById(rid)
-	if err != nil {
-		libhttp.HandleErrorJson(w, err)
-		return
-	}
 
-	if !isUserInRoom(currentUser.Username, room.Members) {
-		notMember := errors.New("You need to be a member of the group to make updates")
-		libhttp.HandleErrorJson(w, notMember)
-		return
-	}
-
-	rs := dal.RoomModel{}
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&rs); err != nil {
-		libhttp.HandleErrorJson(w, err)
-		return
-	}
+	rs := &dal.RoomModel{}
+	rid, rs, err := genericValidation(w, r)
 
 	if db_err := r_db.UpdateRoomNameById(rid, rs.Name); db_err != nil {
 		libhttp.HandleErrorJson(w, db_err)
 		return
 	}
 
-	room, err = r_db.GetRoomById(rid)
+	room, err := r_db.GetRoomById(rid)
 	if err != nil {
 		libhttp.HandleErrorJson(w, err)
 		return
@@ -160,6 +97,42 @@ func UpdateRoomName(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(rj)
 	return
+}
+
+func genericValidation(w http.ResponseWriter, r *http.Request) (rid bson.ObjectId, rs *dal.RoomModel, err error) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if !isUserLoggedIn(w, r) {
+		notAllowed := errors.New("You need to be logged in to perform this action")
+		return "", nil, notAllowed
+	}
+
+	rid, err = getIdFromPath(w, r)
+	if err != nil {
+		return "", nil, err
+	}
+
+	currentUser := getCurrentUser(w, r)
+	db := context.Get(r, "db").(*mgo.Session)
+
+	r_db := dal.NewRoom(db)
+	room, err := r_db.GetRoomById(rid)
+	if err != nil {
+		return "", nil, err
+	}
+
+	if !isUserInRoom(currentUser.Username, room.Members) {
+		notMember := errors.New("You need to be a member of the group to make updates")
+		return "", nil, notMember
+	}
+
+	rs = &dal.RoomModel{}
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(rs); err != nil {
+		return "", nil, err
+	}
+
+	return rid, rs, nil
 }
 
 func isUserInRoom(user string, members []string) bool {
