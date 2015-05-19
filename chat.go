edd713efa688547bb33da57a9d9151b00582672b
Ziyad Parekh <ziyad@1stdibs.com>
2015-05-19 15:14:37 -0400

    User register layer with helper functions and dal

--- chat.go
+++ chat.go
@@ -3,15 +3,21 @@ package main
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/gob"
 	"encoding/json"
 	"errors"
 	"io"
 	"log"
 	"net/http"
+	"regexp"
 	"strings"
 	"time"
 
+	"github.com/carbocation/interpose"
+	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -20,11 +26,17 @@ const (
 	TtlDuration time.Duration = 20 * time.Minute
 )
 
+var ErrClientExists = errors.New("clientdata: Client already exists")
+var ErrUsernameInvalid = errors.New("Error registering user: Invalid username")
+var ErrPasswordInvalid = errors.New("Error registering user: Invalid password")
+var ErrNumberInvalid = errors.New("Error registering user: Invalid number")
+
 type User struct {
 	Id       bson.ObjectId `json:"id" mgo:"_id"`
 	Username string        `json:"username" mgo:"username"`
 	Number   string        `json:"number" mgo:"number"`
 	Token    string        `json:"token" mgo:"token"`
+	Password string        `json:"password" mgo:"password"`
 }
 
 type Group struct {
@@ -43,6 +55,58 @@ type Message struct {
 	Content string    `json:"content" mgo:content`
 	Action  int       `json:"action" mgo:action`
 	Time    time.Time `json:"time" mgo:"time"`
+}
+
+type Base struct {
+	db         *mgo.Session
+	collection string
+	dbName     string
+}
+
+type UserDataClient struct {
+	Base
+}
+
+func NewUserData(db *mgo.Session) *UserDataClient {
+	u := new(UserDataClient)
+	u.db = db
+	u.collection = "users"
+	u.dbName = "superfish"
+	return u
+}
+
+func (u *UserDataClient) GetByUsername(username string) (*User, error) {
+	user := new(User)
+	c := u.db.DB(u.dbName).C(u.collection)
+	err := c.Find(bson.M{"username": username}).One(user)
+	return user, err
+}
+
+func (u *UserDataClient) CreateNewUser(user *User) error {
+	c := u.db.DB(u.dbName).C(u.collection)
+	err := c.Insert(user)
+	return err
+}
+
+func (u *UserDataClient) ClientExists(username string) bool {
+	_, err := u.GetByUsername(username)
+	if err == mgo.ErrNotFound {
+		return false
+	}
+	return true
+}
+
+func (u *UserDataClient) NewUser(user *User) error {
+	exists := u.ClientExists(user.Username)
+	if exists {
+		return ErrClientExists
+	}
+	user.Password = Encrypt(user.Password)
+	err := u.CreateNewUser(user)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func CreateGroup(w http.ResponseWriter, r *http.Request) {
@@ -104,12 +168,67 @@ func Register(w http.ResponseWriter, r *http.Request) {
 		ServerError(w, err)
 		return
 	}
+	if err := ValidateUser(user); err != nil {
+		w.Header().Set("success", "false")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
 	user.Id = bson.NewObjectId()
 	user.Token, _ = generateUserToken(user)
-	// TODO:: save user to db
-	uj, _ := json.Marshal(user)
-	w.WriteHeader(http.StatusOK)
-	w.Write(uj)
+	db := GetMongoSession(r)
+	u := NewUserData(db)
+	err := u.NewUser(user)
+	switch {
+	case err == ErrClientExists:
+		log.Println(err)
+		w.Header().Set("success", "false")
+		w.Header().Set("code", "20")
+		w.Write([]byte(err.Error()))
+	case err != nil:
+		ServerError(w, err)
+	default:
+		uj, _ := json.Marshal(user)
+		w.WriteHeader(http.StatusOK)
+		w.Write(uj)
+	}
+}
+
+func ValidateUser(u *User) error {
+	if !ValidateName(u.Username) {
+		return ErrUsernameInvalid
+	}
+	if !ValidatePassword(u.Password) {
+		return ErrPasswordInvalid
+	}
+	if !ValidateNumber(u.Number) {
+		return ErrNumberInvalid
+	}
+	return nil
+}
+
+//ValidateName returns true if a name is only alphanumeric characters.
+func ValidateName(name string) bool {
+	inv, err := regexp.MatchString("[^[:alnum:]]", name)
+	if err != nil {
+		log.Println("Error in ValidateName: ", err)
+	}
+	return !inv
+}
+
+func ValidatePassword(pswd string) bool {
+	if pswd == "" || len([]rune(pswd)) < 3 {
+		return false
+	}
+	return true
+}
+
+func ValidateNumber(number string) bool {
+	reg := regexp.MustCompile(`^(\([0-9]{3}\)|[0-9]{3})[0-9]{3}[0-9]{4}$`)
+	if number == "" || !reg.MatchString(number) {
+		return false
+	}
+	return true
 }
 
 //ServerError handles server errors writing a response to the client and logging the error.
@@ -158,16 +277,65 @@ func generateFreshToken() (string, error) {
 	return base64.URLEncoding.EncodeToString(token), nil
 }
 
+//encrypt encrypts the password and returns the encrypted version.
+func Encrypt(pword string) string {
+	crypass, err := bcrypt.GenerateFromPassword([]byte(pword), 12)
+	if err != nil {
+		log.Println("Error encrypting pass: ", err)
+	}
+	return string(crypass)
+}
+
 func resetTokenExpiryTime() time.Time {
 	return time.Now().UTC().Add(TtlDuration)
 }
 
-func main() {
+func init() {
+	gob.Register(new(User))
+}
+
+func GetMongoSession(r *http.Request) *mgo.Session {
+	return context.Get(r, "db").(*mgo.Session)
+}
+
+func getDb() *mgo.Session {
+	dsn := "mongodb://localhost"
+	db, err := mgo.Dial(dsn)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return db
+}
+
+func SetDB(db *mgo.Session) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+			context.Set(req, "db", db)
+			next.ServeHTTP(res, req)
+		})
+	}
+}
+
+func middlewareStruct() *interpose.Middleware {
+	middle := interpose.New()
+	middle.Use(SetDB(getDb()))
+	return middle
+}
+
+func routeMux() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/signup", Register).Methods("POST")
 	router.HandleFunc("/group", CreateGroup).Methods("POST")
+	return router
+}
 
-	err := http.ListenAndServe(":8080", router)
+func main() {
+	router := routeMux()
+	middle := middlewareStruct()
+	middle.UseHandler(router)
+
+	err := http.ListenAndServe(":8080", middle)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
